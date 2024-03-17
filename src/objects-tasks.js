32c7cc94eee0bab6c22ffbab3b54473c3c9e2bd0
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-17 16:44:35 +0100

    fix: take out a warning

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -211,7 +211,7 @@ function Rectangle(width, height) {
   this.width = width;
   this.height = height;
 
-  this.getArea = function () {
+  this.getArea = () => {
     return this.width * this.height;
   };
 

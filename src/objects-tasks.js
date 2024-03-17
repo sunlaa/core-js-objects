0181e95bcca721cf5e07f501f9c3a6757757a2e8
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-17 13:36:31 +0100

    feat: solve task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -111,8 +111,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -125,8 +125,14 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const resultArr = [];
+  Object.entries(lettersObject).forEach(([letter, places]) => {
+    places.forEach((index) => {
+      resultArr[index] = letter;
+    });
+  });
+  return resultArr.join('');
 }
 
 /**
